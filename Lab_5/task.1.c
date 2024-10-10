917cf75193d0ba50d808e78f00f44fd4963c8bdd
AntoskovaA <ALINA.ANTOSHKOVA@lnu.edu.ua>
2024-10-10 10:47:25 +0300

    modified task1

--- Lab_5/task.1.c
+++ Lab_5/task.1.c
@@ -10,8 +10,8 @@ int main()
     scanf("%d", &x);
     printf("Введіть число y: ");
     scanf("%d", &y);
+    (x == y) ? printf("x дорівнює y\n") :
+    (x > y) ? printf("x більше за y") : printf("x менше за y");
     
-    int res = (x > y) ? printf("x більше за y") : printf("x менше за y");
-
     return 0;
 }
